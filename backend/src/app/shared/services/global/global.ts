d35254c24128555243fdf982b55244154a7a8fd6
Desarrollo <desarrollo@iMac-de-Desarrollo.local>
2019-05-07 11:09:30 -0400

    SSL

--- backend/src/app/shared/services/global/global.ts
+++ backend/src/app/shared/services/global/global.ts
@@ -1,5 +1,6 @@
 export var GLOBAL = {
     // url: 'http://localhost:3789/api/'
-     url: 'http://lagranja.agendamunicipal.cl:3803/api/'
+    //  url: 'http://lagranja.agendamunicipal.cl:3803/api/'
+    url: 'https://lagranja.agendamunicipal.cl/api/'
 
 }
